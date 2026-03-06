e15e2c5a81b6855f72297be1e9b3fef60a6fe63f
Your Name <you@example.com>
2026-03-06 09:21:25 +0530

    UC9 Added

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,76 +1,19 @@
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class PalindromeCheckerApp {
 
-    static Node head = null;
+    static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end)
+            return true;
 
-    static void add(char data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = newNode;
-    }
+        if (str.charAt(start) != str.charAt(end))
+            return false;
 
-    static Node reverse(Node node) {
-        Node prev = null;
-        Node current = node;
-        Node next;
-
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        return prev;
-    }
-
-    static boolean isPalindrome() {
-        if (head == null || head.next == null) return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-        return true;
+        return isPalindrome(str, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
         String text = "madam";
 
-        for (char c : text.toCharArray()) {
-            add(c);
-        }
-
-        if (isPalindrome())
+        if (isPalindrome(text, 0, text.length() - 1))
             System.out.println(text + " is a Palindrome");
         else
             System.out.println(text + " is not a Palindrome");

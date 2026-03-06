ed769e037c76b271cce8f107728c0d0e4c7b6cce
Your Name <you@example.com>
2026-03-06 09:45:29 +0530

    UC13 Added

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -4,9 +4,8 @@ import java.util.Deque;
 
 public class PalindromeCheckerApp {
 
-    static boolean stackStrategy(String text) {
+    static boolean stackMethod(String text) {
         Stack<Character> stack = new Stack<>();
-
         for (char c : text.toCharArray())
             stack.push(c);
 
@@ -17,9 +16,8 @@ public class PalindromeCheckerApp {
         return true;
     }
 
-    static boolean dequeStrategy(String text) {
+    static boolean dequeMethod(String text) {
         Deque<Character> deque = new ArrayDeque<>();
-
         for (char c : text.toCharArray())
             deque.addLast(c);
 
@@ -30,20 +28,36 @@ public class PalindromeCheckerApp {
         return true;
     }
 
+    static boolean twoPointerMethod(String text) {
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String word = "madam";
-        String strategy = "stack";
 
-        boolean result;
+        long start1 = System.nanoTime();
+        stackMethod(word);
+        long end1 = System.nanoTime();
 
-        if (strategy.equals("stack"))
-            result = stackStrategy(word);
-        else
-            result = dequeStrategy(word);
+        long start2 = System.nanoTime();
+        dequeMethod(word);
+        long end2 = System.nanoTime();
 
-        if (result)
-            System.out.println(word + " is a Palindrome");
-        else
-            System.out.println(word + " is not a Palindrome");
+        long start3 = System.nanoTime();
+        twoPointerMethod(word);
+        long end3 = System.nanoTime();
+
+        System.out.println("Stack Method Time: " + (end1 - start1) + " ns");
+        System.out.println("Deque Method Time: " + (end2 - start2) + " ns");
+        System.out.println("Two Pointer Method Time: " + (end3 - start3) + " ns");
     }
 }
